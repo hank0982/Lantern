93203a80efcc505f05a44710b5c9ae10a601d79e
YuTan9 <s110095@shsh.tw>
2017-10-21 20:37:05 +0800

    RegPre First attempt by tang

--- API/APIs.js
+++ API/APIs.js
@@ -1,14 +1,17 @@
 module.exports = {
     SignUpAPI: {
-        signUpWithEmail(firebase, email, pwd) {
+        signUpWithEmail(firebase, email, pwd, repwd) {
             if (!email.include('@')) {
                 alert('Invalid Email!');
-                return;
+                return 0;
             }
-
-            firebase.auth().createUserWithEmailAndPassword(email, password)
+            if(pwd != repwd){
+                alert('the first pwd and the second is different~');
+                return -1;
+            }
+            firebase.auth().createUserWithEmailAndPassword(email, pwd)
                 .then(function(user) {
-                    LoginAPI.loginWithEmail(firebase, email, passward);
+                    LoginAPI.loginWithEmail(firebase, email, pwd);
                 })
                 .catch(function(error) {
                     var errorCode = error.code;
@@ -21,6 +24,7 @@ module.exports = {
                     }
                     console.log(error);
                 });
+                return 0;
         }
     },
     LoginAPI: {
