4871285f7edd07e670576a0c6365368828c2b767
hank0982 <hank0982@gmail.com>
2017-10-21 23:30:25 +0800

    timeline change

--- FrontEnd/TimeLine.js
+++ FrontEnd/TimeLine.js
@@ -1,101 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Timeline from 'react-native-timeline-listview';
+import { StyleSheet, Text, View, Image ,TouchableHighlight} from 'react-native';
+import SortableListView from 'react-native-sortable-listview'
 import TimeLineStyle from '../StyleSheets/TimeLineStyle';
-export default class TimeLine extends React.Component {
-  constructor(){
-    super()
-    this.onEventPress = this.onEventPress.bind(this)
-    this.renderSelected = this.renderSelected.bind(this)
-    this.renderDetail = this.renderDetail.bind(this)
 
-    this.data = [
-      {
-        time: '09:00', 
-        title: 'Archery Training', 
-        description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ',
-        lineColor:'#009688', 
-        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240340/c0f96b3a-0fe3-11e7-8964-fe66e4d9be7a.jpg'
-      },
-      {
-        time: '10:45', 
-        title: 'Play Badminton', 
-        description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.', 
-        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240405/0ba41234-0fe4-11e7-919b-c3f88ced349c.jpg'
-      },
-      {
-        time: '12:00', 
-        title: 'Lunch', 
-      },
-      {
-        time: '14:00', 
-        title: 'Watch Soccer', 
-        description: 'Team sport played between two teams of eleven players with a spherical ball. ',
-        lineColor:'#009688', 
-        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240419/1f553dee-0fe4-11e7-8638-6025682232b1.jpg'
-      },
-      {
-        time: '16:30', 
-        title: 'Go to Fitness center', 
-        description: 'Look out for the Best Gym & Fitness Centers around me :)', 
-        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
-      }
-    ]
-    this.state = {selected: null}
-  } 
-  static navigationOptions = TimeLineStyle.navigationOptions;
+let data = {
+  hello: { text: 'world' },
+  how: { text: 'are you' },
+  test: { text: 123 },
+  this: { text: 'is' },
+  a: { text: 'a' },
+  real: { text: 'real' },
+  drag: { text: 'drag and drop' },
+  bb: { text: 'bb' },
+  cc: { text: 'cc' },
+  dd: { text: 'dd' },
+  ee: { text: 'ee' },
+  ff: { text: 'ff' },
+  gg: { text: 'gg' },
+  hh: { text: 'hh' },
+  ii: { text: 'ii' },
+  jj: { text: 'jj' },
+  kk: { text: 'kk' },
+}
 
-  onEventPress(data){
-    this.setState({selected: data})
-  }
+let order = Object.keys(data) //Array of keys
 
-  renderSelected(){
-      if(this.state.selected)
-        return <Text style={{marginTop: 5}}>Selected event: {this.state.selected.title} at {this.state.selected.time}</Text>
-  }
-
-  renderDetail(rowData, sectionID, rowID) {
-    let title = <Text style={[styles.title]}>{rowData.title}</Text>
-    var desc = null
-    if(rowData.description && rowData.imageUrl)
-      desc = (
-        <View style={styles.descriptionContainer}>   
-          <Image source={{uri: rowData.imageUrl}} style={styles.image}/>
-          <Text style={[styles.textDescription]}>{rowData.description}</Text>
-        </View>
-      )
-    
-    return (
-      <View style={{flex:1}}>
-        {title}
-        {desc}
-      </View>
-    )
-  }
-
+class RowComponent extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        {this.renderSelected()}
-        <Timeline 
-          style={styles.list}
-          data={this.data}
-          circleSize={20}
-          circleColor='rgba(0,0,0,0)'
-          lineColor='rgb(45,156,219)'
-          timeContainerStyle={{minWidth:52, marginTop: 0}}
-          timeStyle={{textAlign: 'center', backgroundColor:'#ff9797', color:'white', padding:5, borderRadius:13}}
-          descriptionStyle={{color:'gray'}}
-          options={{
-            style:{paddingTop:0}
-          }}
-          innerCircle={'icon'}
-          onEventPress={this.onEventPress}
-          renderDetail={this.renderDetail}
-        />
-      </View>
-    );
+      <TouchableHighlight
+        underlayColor={'#eee'}
+        style={{
+          padding: 25,
+          backgroundColor: '#F8F8F8',
+          borderBottomWidth: 1,
+          borderColor: '#eee',
+        }}
+        {...this.props.sortHandlers}
+      >
+        <Text>{this.props.data.text}</Text>
+      </TouchableHighlight>
+    )
   }
 }
 
-const styles = TimeLineStyle.stylesheet;
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <SortableListView
+        style={{ flex: 1 }}
+        data={data}
+        order={order}
+        onRowMoved={e => {
+          order.splice(e.to, 0, order.splice(e.from, 1)[0])
+          this.forceUpdate()
+        }}
+        renderRow={row => <RowComponent data={row} />}
+      />
+    )
+  }
+}
+export default MyComponent;
+
