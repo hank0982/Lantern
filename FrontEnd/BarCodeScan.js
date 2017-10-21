2eabe944de7b26bf2ae8f627db5ddc6e2017e80d
hank0982 <hank0982@gmail.com>
2017-10-21 15:31:13 +0800

    change style

--- FrontEnd/BarCodeScan.js
+++ FrontEnd/BarCodeScan.js
@@ -35,22 +35,19 @@ export default class BarCodeScan extends Component {
     };
 
     render() {
-        return ( <
-            View style = { styles.container } > {
-                this.state.hasCameraPermission === null ?
-                <
-                Text > Requesting
-                for camera permission < /Text> :
+        return ( <View style = { styles.container } > {
+                this.state.hasCameraPermission === null ? 
+                <Text> Requesting for camera permission </Text> :
                 this.state.hasCameraPermission === false ?
-                <
-                Text > Camera permission is not granted < /Text> : <
-                BarCodeScanner
+                <Text > Camera permission is not granted 
+                </Text> : 
+               <BarCodeScanner
                 onBarCodeRead = { this._handleBarCodeRead }
                 style = {
                     { height: 400, width: 400 } }
                 />
-            } <
-            /View>
+            } 
+            </View>
         );
     }
 }
